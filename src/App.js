791bf75c70024a9729f0303353a1941b939d8837
RubenArthuys <ruben.arthuys@gmail.com>
2020-10-11 15:03:25 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-
 const api = {
   key: "1d74672073b792c77fa94c2025871385",
   base: "https://api.openweathermap.org/data/2.5"
-
 }
-
 function App() {
   return (
     <div className="app">
